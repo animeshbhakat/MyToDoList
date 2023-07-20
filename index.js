4a5c740d43fb1759a9e6eafc99cad9c608986a9d
Animesh Bhakat <bhakat.animesh1024@gmail.com>
2023-07-20 08:57:42 +0530

    updated views path

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", "/views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
