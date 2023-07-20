ace092198266cc1c2d8dc322bc1059d2ebb16e02
Animesh Bhakat <bhakat.animesh1024@gmail.com>
2023-07-20 09:08:31 +0530

    changed express directory variable

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "../public"));
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL, {
